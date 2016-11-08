bb1d91699f596cd100369c771ede7e12c59e709f
Cole Hudson <cole.hudson@wayne.edu>
2016-11-08 15:35:17 -0500

    updating dependencies

--- src/dependencies.php
+++ src/dependencies.php
@@ -17,3 +17,22 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+// Register Twig View helper
+$container['view'] = function ($c) {
+	$settings = $c->get('settings')['renderer'];
+    $view = new \Slim\Views\Twig($settings['template_path'], [
+        'cache' => 'path/to/cache'
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
+};
+
+$container['guzzle'] = function ($c) {
+    $guzzle = new \GuzzleHttp\Client();
+    return $guzzle;
+};
