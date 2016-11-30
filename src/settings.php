349360deb8229c09b7938c661fc733cfb5c2bbd7
Cole Hudson <cole.hudson@wayne.edu>
2016-11-30 12:11:06 -0500

    new setting-allow route to be available in middleware

--- src/settings.php
+++ src/settings.php
@@ -3,6 +3,7 @@ return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+        'determineRouteBeforeAppMiddleware' => false, // Only set this if you need access to route within middleware
 
         // Renderer settings
         'renderer' => [
